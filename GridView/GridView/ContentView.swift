fd1b6c605ccd95b17352c0751b3f452189383a08
KimSangHyun <kshy1019@naver.com>
2022-02-01 17:33:07 +0900

    Picker 구현

--- GridView/GridView/ContentView.swift
+++ GridView/GridView/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var gridType: GridType = GridType.single
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       GridTypePicker(gridType: $gridType)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct GridTypePicker:View {
+
+    @Binding var gridType: GridType
+    
+    var body: some View {
+        Picker("grid", selection: $gridType){
+            ForEach(GridType.allCases, id: \.self){ type in
+                switch type {
+                case .single:
+                    Image(systemName: "rectangle.grid.1x2")
+                case .double:
+                    Image(systemName: "square.grid.2x2")
+                case .triple:
+                    Image(systemName: "square.grid.3x2")
+                case .adaptive:
+                    Image(systemName: "square.grid.4x3.fill")
+                }
+            }
+        }.pickerStyle(SegmentedPickerStyle())
     }
 }
---
+++ GridView/GridView/GridType.swift
@@ -0,0 +1,15 @@
+//
+//  GridType.swift
+//  GridView
+//
+//  Created by 김상현 on 2022/02/01.
+//
+
+import Foundation
+
+enum GridType: CaseIterable{
+    case single
+    case double
+    case triple
+    case adaptive
+}
