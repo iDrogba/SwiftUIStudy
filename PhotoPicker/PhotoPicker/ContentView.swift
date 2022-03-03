eb42bf0b70498a9ae51947edc53f57acf7c151f9
KimSangHyun <74828662+HimHeys@users.noreply.github.com>
2022-03-03 21:06:13 +0900

    PhotoPicker 완성

--- PhotoPicker/PhotoPicker/ContentView.swift
+++ PhotoPicker/PhotoPicker/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 import PhotosUI
 
 struct ContentView: View {
+    
     @State private var isPresenPicker = false
     @State private var images = [UIImage]()
+    
     var body: some View {
         VStack{
             Button("Select Image"){
@@ -20,42 +22,77 @@ struct ContentView: View {
             List{
                 ForEach(images, id:\.self){ image in
                     Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: UIScreen.main.bounds.size.width/1.5, height: UIScreen.main.bounds.size.height/3)
+                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 10))
+                       
+                        
                 }
             }
         }
         .sheet(isPresented: $isPresenPicker, onDismiss: nil, content: {
-            //Picker
+            //Picker 뿌려줄 것
+            ImagePicker(isPresent: $isPresenPicker, images: $images)
         })
     }
 }
 
+//viewcontroller 가져올때 representable
 struct ImagePicker: UIViewControllerRepresentable {
     
     @Binding var isPresent: Bool
+    @Binding var images: [UIImage]
     
     func makeCoordinator() -> Coordinator {
-        <#code#>
+        return ImagePicker.Coordinator(picker: self) //구조체 자체를 넣어줌
     }
     
     func makeUIViewController(context: Context) -> some UIViewController {
         
         var config = PHPickerConfiguration()
-        config.selectionLimit = 0
-        config.filter = .images
+        config.selectionLimit = 0  //피커 선택 개수 제한
+        config.filter = .images  //미디어 종류
         
         let picker = PHPickerViewController(configuration: config)
         
-//        picker.delegate = context.coordinator
+        picker.delegate = context.coordinator
         
         return picker
     }
+    
     class Coordinator: PHPickerViewControllerDelegate{
+        
+        var picker: ImagePicker
+        
+        init(picker: ImagePicker){
+            self.picker = picker
+        }
+        
         func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-            <#code#>
+            
+            self.picker.isPresent = false // picker 내리기
+            self.picker.images.removeAll() // 배열을 모두 비우고 새로 가져오도록 함
+
+            for img in results {
+                if img.itemProvider.canLoadObject(ofClass: UIImage.self){
+                    img.itemProvider.loadObject(ofClass: UIImage.self) { loadImage, error in
+                        
+                        guard let hasImage = loadImage else{
+                            print("Empty Image")
+                            return
+                        }
+                        self.picker.images.append(hasImage as! UIImage)
+                    }
+                }else{
+                    print("failed to load")
+                }//이미지를 로드할 수 있는지 확인해줌. 못 불러오는 이미지 있을 수 있으므로 이거 사용하는건 국룰임.
+            }
         }
     }
+    
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-        <#code#>
+        
     }
 }
 
