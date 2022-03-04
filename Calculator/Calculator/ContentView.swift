0c43895522de5a617b99320c652d42e2e5c8bea9
KimSangHyun <74828662+HimHeys@users.noreply.github.com>
2022-03-04 20:57:03 +0900

    Calculator 버튼 Text 삽입

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -11,8 +11,15 @@ struct ContentView: View {
     
     @State private var geometryCircleHeight : CGFloat = 50
     
+    let data = [
+        ["AC","+-","%","/"],
+        ["7","8","9","*"],
+        ["4","5","6","-"],
+        ["1","2","3","+"],
+        ["0",".","="]
+    ]
+    
     var body: some View {
-        
         VStack(spacing:10){
         
             Spacer(minLength: 50)
@@ -22,46 +29,56 @@ struct ContentView: View {
                 .padding(.trailing, 10)
                 .frame(width: UIScreen.main.bounds.size.width , alignment: .trailing)
             
-            ForEach(0..<4){_ in
+            ForEach(0..<4){ indexI in
                 HStack(spacing: 10){
-                    ForEach(0..<4){ _ in
-                        CalculationButton()
+                    ForEach(0..<4){ indexJ in
+                        
+                        CalculationButton(buttonName: data[indexI][indexJ])
                             .aspectRatio(1, contentMode: .fit)
+                        
                     }
                 }.padding(.horizontal, 10)
             }
-            
 
             HStack(spacing: 10){
                 GeometryReader { geometry in
                     
                     Capsule()
                         .foregroundColor(Color.orange)
+                        .overlay{
+                            Text("\(data[4][0])")
+                                .font(.largeTitle)
+                        }
                         .aspectRatio(CGSize(width: geometry.size.height * 2 + 10, height: geometry.size.height), contentMode: .fit)
                         .onAppear {
                             self.geometryCircleHeight = geometry.size.height
                         }
+                    
                 }
                
-                CalculationButton()
+                CalculationButton(buttonName: data[4][1])
                     .aspectRatio(1, contentMode: .fit)
-                CalculationButton()
+                
+                CalculationButton(buttonName: data[4][2])
                     .aspectRatio(1, contentMode: .fit)
-            }
-            .aspectRatio(CGSize(width:  geometryCircleHeight * 4 + 30, height: geometryCircleHeight), contentMode: .fit)
+                
+            }.aspectRatio(CGSize(width:  geometryCircleHeight * 4 + 30, height: geometryCircleHeight), contentMode: .fit)
             .padding(.horizontal, 10)
+            
         }.padding(.bottom, 10)
-        
     }
+    
 }
 
 struct CalculationButton: View {
+    
+    var buttonName = ""
     
     var body: some View {
         Circle()
             .foregroundColor(Color.orange)
             .overlay (
-                Text("??")
+                Text("\(buttonName)")
                     .font(.largeTitle)
             )
     }
