cc2e49aa6d1701f10747315aa1cd4aebfb135e95
KimSangHyun <74828662+iDrogba@users.noreply.github.com>
2022-04-09 17:15:52 +0900

    FireBaseDB init

---
+++ FireBaseDB/FireBaseDB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FireBaseDB
+//
+//  Created by 김상현 on 2022/04/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FireBaseDB/FireBaseDB/FireBaseDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseDBApp.swift
+//  FireBaseDB
+//
+//  Created by 김상현 on 2022/04/09.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
